e027fe7316b16165624454b42e2e62739035afdf
hemintang <hemintang@4399inc.com>
2017-04-20 16:21:32 +0800

    数据库名改成用传参方式

--- src/main/scala/com/xm4399/enumeration/Table.scala
+++ src/main/scala/com/xm4399/enumeration/Table.scala
@@ -5,15 +5,13 @@ package com.xm4399.enumeration
   */
 object Table extends Enumeration{
   type T = HTable
-  val T_adgame_ctr = new HTable("hmt_db", "t_adgame_ctr", "datekey")
-  val T_adgame_ctr_midres = new HTable("hmt_db", "t_adgame_ctr_midres", "datekey")
-  val Reality_search_show = new HTable("datamarket_adgame", "reality_search_show", "datekey")
+  val T_adgame_ctr = new HTable("t_adgame_ctr", "datekey")
+  val T_adgame_ctr_midres = new HTable("t_adgame_ctr_midres", "datekey")
+  val Reality_search_show = new HTable("reality_search_show", "datekey")
 
 }
 
-class HTable(pDatabaseName: String, pTableName: String, pPartitionQualifier: String){
-
-  def databaseName: String = pDatabaseName
+class HTable(pTableName: String, pPartitionQualifier: String){
 
   def tableName: String = pTableName
 
